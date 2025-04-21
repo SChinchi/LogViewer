0aea456ded3cdd718fd118867143da87015502ca
SChinchi <57867641+SChinchi@users.noreply.github.com>
2025-04-21 08:44:44 +0100

    Rename the compiled output folders and file names

--- build_all.ps1
+++ build_all.ps1
@@ -4,7 +4,7 @@ flutter pub get
 flutter build windows
 $windowsPath = ".\build\windows\x64\runner\Release"
 robocopy .\dependencies $windowsPath sqlite3.dll
-robocopy $windowsPath .\build\binaries\windows /e
+robocopy $windowsPath .\build\binaries\LogViewer_win /e
 
 flutter build apk --split-per-abi
-robocopy .\build\app\outputs\flutter-apk\ .\build\binaries\ *.apk
+robocopy .\build\app\outputs\apk\release\ .\build\binaries\ *.apk
