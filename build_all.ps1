c94333b21e6a9172b87aed7423ab96e13ea99fbd
SChinchi <57867641+SChinchi@users.noreply.github.com>
2025-10-18 00:00:00 +0100

    Add building pipeline for web

--- build_all.ps1
+++ build_all.ps1
@@ -8,3 +8,7 @@ robocopy $windowsPath .\build\binaries\LogViewer_win /e
 
 flutter build apk --split-per-abi
 robocopy .\build\app\outputs\apk\release\ .\build\binaries\ *.apk
+
+dart run isolate_manager:generate
+flutter build web
+robocopy .\build\web .\build\binaries\LogViewer_web
