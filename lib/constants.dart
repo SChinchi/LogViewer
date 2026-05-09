SChinchi/LogViewer

commit 500c1b76ee0045af2c6d597ab7291140f3ec1bae

Capture the BepInEx "[Loading <modname>]" message for mods
The "TS Manifest: <TS guid>" line that usually precedes it for RoR2 BepInEx
might be missing and if we are only relying on this pattern, we can miss out
on loaded mods. As well as for still collecting any loaded mods for non-RoR2
BepInEx versions. Having said that, we still totally rely on the TS Manifest
line to match for deprecation/update mod data from the Thunderstore API and
without it, any relevant features won't work.

lib/constants.dart
@@ -17,6 +17,8 @@ class Constants {
   static const buttonOk = 'OK';
   static const buttonCancel = 'Cancel';
 
+  static const noManifestModName = 'Unknown';
+
   static const logSeverity = [
     'Fatal',
     'Error',

lib/log_parser.dart
@@ -29,7 +29,7 @@ class Event {
   late int index;
   late int lineCount;
   int repeat = 0;
-  String? modName;
+  int? modIndex;
   final controller = ExpandableController();
 
   Event(String text, RegExpMatch match) {
@@ -43,7 +43,7 @@ class Event {
     index = data.index;
     lineCount = data.lineCount;
     repeat = data.repeat;
-    modName = data.modName;
+    modIndex = data.modIndex;
   }
 
   Event.clone(Event event) {
@@ -56,7 +56,7 @@ class Event {
     index = event.index;
     lineCount = event.lineCount;
     repeat = repeat;
-    modName = event.modName;
+    modIndex = event.modIndex;
   }
 
   Event.fromJson(Map<String, dynamic> data) {
@@ -70,7 +70,17 @@ class Event {
     index = data['index'] as int;
     lineCount = data['lineCount'] as int;
     repeat = data['repeat'] as int;
-    modName = data['modName'];
+    modIndex = data['modIndex'] as int?;
+  }
+
+  String? get modName {
+    if (modIndex != null) {
+      final mods = Logger.modManager.mods;
+      if (modIndex! >= 0 && modIndex! < mods.length) {
+        return mods[modIndex!].name;
+      }
+    }
+    return null;
   }
 }
 
@@ -243,7 +253,7 @@ class Logger {
         else {
           mod.isLatestVersion = mod.version.toString() == entry.latestVersion;
         }
-        mod.isProblematic = problematicModlist.contains(mod.guid);
+        mod.isProblematic = problematicModlist.contains(mod.fullName) || mod.isMissingManifest;
       }
       else {
         toUpdate.add(mod.fullName);
@@ -277,7 +287,7 @@ class Logger {
                           .difference(cutOffDate)
                           .isNegative
                       && !mod.isDeprecated;
-                  mod.isProblematic = problematicModlist.contains(fullName);
+                  mod.isProblematic = problematicModlist.contains(fullName) || mod.isMissingManifest;
                   mod.isLatestVersion = mod.version.toString() == latestVersion;
                 }
                 final entry = Entry(
@@ -307,9 +317,11 @@ class Logger {
     if (data['success'] != true) {
       return;
     }
-    for (final mod in data['mods']) {
-      Logger.modManager.add(Mod(mod));
+    for (final modData in data['mods']) {
+      final mod = List<String>.from(modData);
+      Logger.modManager.add(Mod(mod[0], mod[1]));
     }
+    Logger.modManager.updateAmbiguousMods();
     getAllModsStatus();
 
     for (final line in data['summary']) {

lib/screens/console/tabs/modlist_tab.dart
@@ -117,7 +117,7 @@ class _ModListPageState extends State<ModListPageState> with AutomaticKeepAliveC
                           final mod = mods[index];
                           return GestureDetector(
                             child: Text(
-                              mod.guid,
+                              mod.name,
                               textAlign: TextAlign.left,
                               style: TextStyle(
                                 color: mod.isDeprecated ? Colors.red
@@ -170,13 +170,15 @@ class _ModListPageState extends State<ModListPageState> with AutomaticKeepAliveC
                             final stringBuffer = StringBuffer('profileName: ${Constants.modProfileName}\n');
                             stringBuffer.writeln('mods:');
                             for (var mod in Logger.modManager.mods) {
-                              final version = mod.version;
-                              stringBuffer.writeln('  - name: ${mod.fullName}');
-                              stringBuffer.writeln('    version:');
-                              stringBuffer.writeln('      major: ${version.major}');
-                              stringBuffer.writeln('      minor: ${version.minor}');
-                              stringBuffer.writeln('      patch: ${version.patch}');
-                              stringBuffer.writeln('    enabled: true');
+                              if (!mod.isMissingManifest) {
+                                final version = mod.version;
+                                stringBuffer.writeln('  - name: ${mod.fullName}');
+                                stringBuffer.writeln('    version:');
+                                stringBuffer.writeln('      major: ${version.major}');
+                                stringBuffer.writeln('      minor: ${version.minor}');
+                                stringBuffer.writeln('      patch: ${version.patch}');
+                                stringBuffer.writeln('    enabled: true');
+                              }
                             }
                             final fileHandle = RamFileHandle.asWritableRamBuffer();
                             final fileStream = OutputFileStream.toRamFile(fileHandle);
@@ -209,7 +211,7 @@ class _ModListPageState extends State<ModListPageState> with AutomaticKeepAliveC
                           child: const Icon(Icons.copy),
                           onPressed: () async {
                             final text = Logger.modManager.filteredMods.map((m) =>
-                            m.guid).join('\n');
+                            m.name).join('\n');
                             await Clipboard.setData(ClipboardData(text: text));
                           },
                         ),

lib/parser.dart
@@ -15,7 +15,8 @@ final eventPattern = RegExp('(.*)\\[(${Constants.logSeverity.join('|')})\\s*:\\s
 // The reason for this is that this class cannot contain a Color reference,
 // so this is mainly used for parsing and the other for populating data.
 class Event {
-  static final _modPattern = RegExp(r'^TS Manifest: (.*)');
+  static final _tsModPattern = RegExp(r'^TS Manifest: (.*)');
+  static final _bepInModPattern = RegExp(r'^Loading \[(.*)\]');
 
   late int severity;
   late String source;
@@ -26,7 +27,11 @@ class Event {
   int index = 0;
   late int lineCount;
   int repeat = 0;
-  String? modName;
+  int? modIndex;
+
+  // These are not serialised directly and used for detecting when a mod loads
+  String? tsModName;
+  String? bepInModName;
 
   Event(String text, RegExpMatch match) {
     severity = Constants.logSeverity.indexOf(match.group(2)!);
@@ -41,9 +46,17 @@ class Event {
       color = _yellow;
     }
     lineCount = fullString.split('\n').length;
-    final modPattern = _modPattern.firstMatch(match.group(4)!);
-    if (modPattern != null) {
-      modName = modPattern.group(1);
+
+    // Check for loading mod pattern
+    if (source == 'BepInEx') {
+      final tsModPattern = _tsModPattern.firstMatch(match.group(4)!);
+      if (tsModPattern != null) {
+        tsModName = tsModPattern.group(1)!;
+      }
+      final bepInModPattern = _bepInModPattern.firstMatch(match.group(4)!);
+      if (bepInModPattern != null) {
+        bepInModName = bepInModPattern.group(1)!;
+      }
     }
   }
 
@@ -58,7 +71,7 @@ class Event {
       'index': index,
       'lineCount': lineCount,
       'repeat': repeat,
-      'modName': modName,
+      'modIndex': modIndex,
     };
   }
 }
@@ -67,7 +80,7 @@ class Parser {
   static final eventPattern = RegExp('(.*)\\[(${Constants.logSeverity.join('|')})\\s*:\\s*(.*?)\\] (.*)');
 
   final summary = <List<dynamic>>[];
-  final mods = <String>[];
+  final mods = <List<String>>[];
   final events = <Event>[];
 
   void _addEvent(String text) {
@@ -81,11 +94,28 @@ class Parser {
       events.last.repeat++;
       return;
     }
+    final previousEvent = events.isNotEmpty ? events.last : null;
     final event = Event(text, match);
     event.index = events.length;
     events.add(event);
-    if (event.modName != null) {
-      mods.add(event.modName!);
+
+    // For loading mods the pattern we generally match is:
+    //   [Info : BepInEx] TS Manifest: <tsModName>
+    //   [Info : BepInEx] Loading [<bepInModName>]
+    // However, it is possible for either of these to be missing due to muted logs.
+    // The manifest one can also be missing if the mod has been installed manually,
+    // so we tag those as unknown accordingly.
+    if (previousEvent != null && previousEvent.tsModName != null) {
+      previousEvent.modIndex = mods.length;
+      if (event.bepInModName != null) {
+        event.modIndex = mods.length;
+        mods.add([previousEvent.tsModName!, event.bepInModName!]);
+      } else {
+        mods.add([previousEvent.tsModName!, '']);
+      }
+    } else if (event.bepInModName != null) {
+      event.modIndex = mods.length;
+      mods.add([Constants.noManifestModName, event.bepInModName!]);
     }
   }
 
@@ -126,6 +156,7 @@ class Parser {
       final sb = StringBuffer(lines[0]);
       for (final line in lines.sublist(1, lines.length)) {
         final match = eventPattern.firstMatch(line);
+        // If the new line begins a new log message, the buffer is complete with the previous one
         if (match != null) {
           _addEvent(sb.toString().trimRight());
           sb.clear();
@@ -140,6 +171,7 @@ class Parser {
           }
         }
       }
+      // End of file, flush last message in the buffer
       if (sb.isNotEmpty) {
         _addEvent(sb.toString().trimRight());
       }

lib/providers/mod_manager.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/foundation.dart';
+import 'package:log_viewer/constants.dart';
+import 'package:log_viewer/settings.dart';
 
 class Mod {
-  String guid;
+  late String guid;
+  late String bepInName;
   late String fullName;
   late Version version;
   late int index;
@@ -10,8 +13,9 @@ class Mod {
   bool isProblematic = false;
   bool isSelected = false;
   bool isLatestVersion = true;
+  bool isUnique = true;
 
-  Mod(this.guid) {
+  Mod(this.guid, this.bepInName) {
     var pattern = RegExp(r'^(.*)-(\d+).(\d+).(\d+)');
     var name = pattern.firstMatch(guid);
     if (name != null) {
@@ -23,6 +27,15 @@ class Mod {
       version = Version('1', '0', '0');
     }
   }
+
+  String get name {
+    if (Settings.getUseModManifest()) {
+      return isUnique ? fullName : '$fullName [$bepInName]';
+    }
+    return bepInName;
+  }
+
+  bool get isMissingManifest => Settings.getUseModManifest() ? guid == Constants.noManifestModName : false;
 }
 
 class Version {
@@ -86,7 +99,7 @@ class ModManager with ChangeNotifier {
         || (_category == ModCategory.Deprecated && mod.isDeprecated)
         || (_category == ModCategory.Old && mod.isOld && !mod.isDeprecated)
         || (_category == ModCategory.Problematic && mod.isProblematic)) {
-      return _searchString.pattern.isEmpty || mod.guid.contains(_searchString);
+      return _searchString.pattern.isEmpty || mod.name.contains(_searchString);
     }
     return false;
   }
@@ -143,6 +156,16 @@ class ModManager with ChangeNotifier {
       }
       isInSelectionMode = false;
       notifyListeners();
+    }
+  }
+
+  void updateAmbiguousMods() {
+    for (final mod in mods) {
+      if (mod.isMissingManifest || _nameToMod[mod.fullName]!.length > 1) {
+        // Manual mods are not updated when we fetch Thunderstore data, so we do it... manually
+        mod.isProblematic = mod.isMissingManifest;
+        mod.isUnique = false;
+      }
     }
   }
 

lib/screens/settings_screen.dart
@@ -13,6 +13,7 @@ class SettingsScreen extends StatefulWidget {
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  var _useModManifest = Settings.getUseModManifest();
   var _cutOffDateEnabled = Settings.getUseCutOffDate();
   final _whitelistTextController = TextEditingController();
   final _problematicTextController = TextEditingController();
@@ -63,6 +64,24 @@ class _SettingsScreenState extends State<SettingsScreen> {
                   SettingsSection(
                     title: Constants.settingsSectionMods,
                     tiles: [
+                      ListTile(
+                        title: const Text('Use Mod Manifest'),
+                        trailing: Switch(
+                          value: _useModManifest,
+                          onChanged: (bool value) async {
+                            await Settings.setUseModManifest(value);
+                            setState(() {
+                              _useModManifest = value;
+                            });
+                          },
+                        ),
+                        onTap: () async {
+                          await Settings.setUseModManifest(!_useModManifest);
+                          setState(() {
+                            _useModManifest = !_useModManifest;
+                          });
+                        },
+                      ),
                       ListTile(
                         title: const Text('Use Old Date Threshold'),
                         trailing: Switch(

lib/settings.dart
@@ -4,6 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'log_parser.dart';
 
 class Settings {
+  static const keyUseModManifest = 'use_mod_manifest';
   static const keyUseCutOffDate = 'use_cut_off_date';
   static const keyCutOffDate = 'cut_off_date';
   static const keyDeprecatedAndOldWhitelist = 'deprecated_and_old_whitelist';
@@ -12,6 +13,7 @@ class Settings {
   static const keyTextSizeCopyThreshold = 'text_size_copy_threshold';
 
   static late SharedPreferencesWithCache _prefs;
+  static late bool _useModManifest;
   static late bool _useCutOffDate;
   static DateTime? _cutOffDate;
   static late List<String> _deprecatedAndOldWhitelist;
@@ -21,12 +23,20 @@ class Settings {
 
   static Future<void> init() async {
     _prefs = await SharedPreferencesWithCache.create(cacheOptions: const SharedPreferencesWithCacheOptions());
+    _useModManifest = _prefs.getBool(keyUseModManifest) ?? true;
     _useCutOffDate = _prefs.getBool(keyUseCutOffDate) ?? false;
     _cutOffDate = DateTime.tryParse(_prefs.getString(keyCutOffDate) ?? '');
     _deprecatedAndOldWhitelist = _prefs.getStringList(keyDeprecatedAndOldWhitelist) ?? [];
     _problematicModlist = _prefs.getStringList(keyProblematicModlist) ?? [];
     _consoleEventMaxLines = _prefs.getInt(keyConsoleEventMaxLines) ?? 7;
     _textSizeCopyThreshold = _prefs.getInt(keyTextSizeCopyThreshold) ?? 2000;
+  }
+
+  static bool getUseModManifest() => _useModManifest;
+
+  static Future<void> setUseModManifest(bool value) async {
+    _useModManifest = value;
+    await _prefs.setBool(keyUseModManifest, value);
   }
 
   static Future<void> setUseCutOffDate(bool value) async {

commit dc5d9c6e6d6dec19fc58c8beb9b7a93c25ff498e

Add check for network permission and internet access
Also fixes a bug introduced by 500c1b7 where problematic
mods were not checked against their guid.

lib/constants.dart
@@ -55,4 +55,6 @@ class Constants {
   static const modProfileName = 'Made with LogViewer';
   static const exportFilename = 'export.zip';
   static const emptyModList = 'Cannot create a profile code without a mod list.';
+
+  static const noConnectionError = 'No internet permission or connection with internet access.';
 }

lib/log_parser.dart
@@ -253,7 +253,7 @@ class Logger {
         else {
           mod.isLatestVersion = mod.version.toString() == entry.latestVersion;
         }
-        mod.isProblematic = problematicModlist.contains(mod.fullName) || mod.isMissingManifest;
+        mod.isProblematic = problematicModlist.contains(mod.guid) || mod.isMissingManifest;
       }
       else {
         toUpdate.add(mod.fullName);
@@ -266,7 +266,6 @@ class Logger {
       return;
     }
 
-    // TODO: Ensure network permissions are granted and catch any network errors
     return Future(() => {
       http.get(Uri.parse('https://thunderstore.io/api/v1/package/')).then((response) {
         if (response.statusCode == 200) {
@@ -287,7 +286,7 @@ class Logger {
                           .difference(cutOffDate)
                           .isNegative
                       && !mod.isDeprecated;
-                  mod.isProblematic = problematicModlist.contains(fullName) || mod.isMissingManifest;
+                  mod.isProblematic = problematicModlist.contains(mod.guid) || mod.isMissingManifest;
                   mod.isLatestVersion = mod.version.toString() == latestVersion;
                 }
                 final entry = Entry(
@@ -302,6 +301,32 @@ class Logger {
                 if (toUpdate.isEmpty) {
                   break;
                 }
+              }
+            }
+          }
+        }
+        Diagnostics.collectOutdatedMods();
+        modManager.recalculateFilteredMods();
+      },
+      onError: (e) {
+        // Fallback to assigning any old values we still have in the database
+        for (final fullName in toUpdate) {
+          final entry = query[fullName];
+          if (entry != null) {
+            final modPlugins = Logger.modManager.getModPlugins(fullName);
+            if (modPlugins != null) {
+              for (final mod in modPlugins) {
+                final whitelisted = deprecatedAndOldWhitelist.contains(mod.fullName);
+                mod.isDeprecated = !whitelisted && entry.isDeprecated == 1;
+                mod.isOld = !whitelisted
+                    && cutOffDate != null
+                    && DateTime
+                        .parse(entry.dateTs)
+                        .difference(cutOffDate)
+                        .isNegative
+                    && !mod.isDeprecated;
+                mod.isProblematic = problematicModlist.contains(mod.guid) || mod.isMissingManifest;
+                mod.isLatestVersion = mod.version.toString() == entry.latestVersion;
               }
             }
           }

lib/screens/console/tabs/modlist_tab.dart
@@ -1,5 +1,6 @@
 import 'dart:collection';
 import 'dart:convert';
+import 'dart:io';
 
 import 'package:archive/archive_io.dart';
 import 'package:flutter/material.dart';
@@ -166,7 +167,6 @@ class _ModListPageState extends State<ModListPageState> with AutomaticKeepAliveC
                                 return;
                               }
                             }
-                            // TODO: Ensure network permissions are granted and catch any network errors
                             final stringBuffer = StringBuffer('profileName: ${Constants.modProfileName}\n');
                             stringBuffer.writeln('mods:');
                             for (var mod in Logger.modManager.mods) {
@@ -190,17 +190,24 @@ class _ModListPageState extends State<ModListPageState> with AutomaticKeepAliveC
                             await fileStream.close();
 
                             final data = '#r2modman\n${base64Encode(fileStream.getBytes())}';
-                            final post = await http.post(
-                              Uri.parse('https://thunderstore.io/api/experimental/legacyprofile/create/'),
-                              headers: {
-                                'Content-Type': 'application/octet-stream',
-                              },
-                              body: data,
-                            );
-                            final message = post.statusCode == 200
-                                ? json.decode(post.body)['key']
-                                : 'Error: ${post.statusCode}';
-                            await Clipboard.setData(ClipboardData(text: message));
+                            try {
+                              final post = await http.post(
+                                Uri.parse('https://thunderstore.io/api/experimental/legacyprofile/create/'),
+                                headers: {
+                                  'Content-Type': 'application/octet-stream',
+                                },
+                                body: data,
+                              );
+                              final message = post.statusCode == 200
+                                  ? json.decode(post.body)['key']
+                                  : 'Error: ${post.statusCode}';
+                              await Clipboard.setData(ClipboardData(text: message));
+                            } on SocketException {
+                              if (context.mounted) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                    const SnackBar(content: Text(Constants.noConnectionError)));
+                              }
+                            }
                           },
                         ),
                       ),